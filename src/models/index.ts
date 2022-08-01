859763c379ef5f8d6fb51b68d49a7d53273d84f4
francobenitez <franco.benitez@hogarthww.com>
2022-08-01 10:04:11 -0300

    chore: config sequelizerc & add tag & comment models

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,6 +3,8 @@ import config from "../config/index.js";
 import { Database } from "../types";
 import { User } from "./User.js";
 import { Article } from "./Article.js";
+import { Tag } from "./Tag.js";
+import { Comment } from "./Comment.js";
 
 export const db: Database = {};
 
@@ -16,26 +18,28 @@ db.Sequelize = Sequelize;
 
 db.user = User(sequelize, Sequelize);
 db.article = Article(sequelize, Sequelize);
+db.comment = Comment(sequelize, Sequelize);
+db.tag = Tag(sequelize, Sequelize);
 
 db.article.belongsTo(db.user, {
   foreignKey: "authorId",
   as: "user",
 });
 
-// db.article.belongsToMany(db.tag, {
-//   through: "article_tag",
-//   as: "tags",
-//   foreignKey: "article_id",
-// });
+db.article.belongsToMany(db.tag, {
+  through: "article_tag",
+  as: "tags",
+  foreignKey: "article_id",
+});
 
-// db.tag.belongsToMany(db.article, {
-//   through: "article_tag",
-//   as: "articles",
-//   foreignKey: "tag_id",
-// });
+db.tag.belongsToMany(db.article, {
+  through: "article_tag",
+  as: "articles",
+  foreignKey: "tag_id",
+});
 
-// db.article.hasMany(db.comment, { as: "comments" });
-// db.comment.belongsTo(db.article, {
-//   foreignKey: "articleId",
-//   as: "article",
-// });
+db.article.hasMany(db.comment, { as: "comments" });
+db.comment.belongsTo(db.article, {
+  foreignKey: "articleId",
+  as: "article",
+});
