fddec23fe0485923dbd61865cb332e86029b8862
francobenitez <franco.benitez@hogarthww.com>
2022-08-01 09:52:15 -0300

    chore: remove hello world

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,9 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use("/api/", routes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(PORT, () => {
   return console.log(`Express is listening at http://localhost:${PORT}`);
