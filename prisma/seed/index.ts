cd504aa65a682d971498e02271e73f334381b1a9
francobenitez <franco.benitez@hogarthww.com>
2022-08-03 00:07:48 -0300

    refactor: setup programmatic seeder

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,16 +1,30 @@
 import { PrismaClient } from "@prisma/client";
 import { userData } from "./users";
+import { roleData } from "./roles";
+import { permissionData } from "./permissions";
 
 const prisma = new PrismaClient();
 
 async function main() {
   console.log(`Start seeding ...`);
-  for (const u of userData) {
-    const user = await prisma.users.create({
-      data: u,
-    });
-    console.log(`Created user with id: ${user.id}`);
-  }
+  const seedData = async (
+    dataType: any,
+    dataModel: any,
+    modelString: string
+  ) => {
+    console.log(`Seeding ${modelString} ...`);
+    const model = dataModel;
+    for (const u of dataType) {
+      const data = await model.create({
+        data: u,
+      });
+      console.log(`Created ${modelString.slice(0, -1)} with id: ${data.id}`);
+    }
+  };
+
+  await seedData(userData, prisma.users, "users");
+  await seedData(roleData, prisma.roles, "roles");
+  await seedData(permissionData, prisma.permissions, "permissions");
   console.log(`Seeding finished.`);
 }
 
