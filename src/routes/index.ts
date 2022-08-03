1ead9f6bdeda6d53af9df85273fc130281d04e97
francobenitez <franco.benitez@hogarthww.com>
2022-08-02 22:52:18 -0300

    feat: setup sendresponse util

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,11 @@
 import express from "express";
-// import { sendErrorResponse } from "../utils/sendResponse";
+import { sendErrorResponse } from "../utils/sendResponse";
 import articlesRouter from "./articlesRouter";
 import authRouter from "./authRouter";
 import commentsRouter from "./commentsRouter";
 import tagsRouter from "./tagsRouter";
 
 export default (app: any) => {
-  // app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
   app.use("/api", authRouter);
@@ -14,5 +13,7 @@ export default (app: any) => {
   app.use("/api", commentsRouter);
   app.use("/api", tagsRouter);
 
-  // app.all('*', (req, res) => sendErrorResponse(res, 404, 'Route does not exist'));
+  app.all("*", (req: any, res: any) =>
+    sendErrorResponse(res, 404, "Route does not exist")
+  );
 };
--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -1,11 +1,21 @@
-export const sendErrorResponse = (res, code, errorMessage, e = null) =>
+export const sendErrorResponse = (
+  res: any,
+  code: any,
+  errorMessage: any,
+  e: any = null
+) =>
   res.status(code).send({
     status: "error",
     error: errorMessage,
     e: e?.toString(),
   });
 
-export const sendSuccessResponse = (res, code, data, message = "Successful") =>
+export const sendSuccessResponse = (
+  res: any,
+  code: any,
+  data: any,
+  message = "Successful"
+) =>
   res.status(code).send({
     status: "success",
     data,
