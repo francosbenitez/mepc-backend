3a9740885b58b0ba7a5b985390f41380aa9b56c2
francobenitez <franco.benitez@hogarthww.com>
2022-08-03 17:51:33 -0300

    fix: clean up canAccess

--- src/middlewares/canAccess.ts
+++ src/middlewares/canAccess.ts
@@ -2,28 +2,11 @@ import { sendErrorResponse } from "../utils/sendResponse";
 import { PrismaClient } from "@prisma/client";
 import type { Request, Response, NextFunction } from "express";
 const prisma = new PrismaClient();
-const Permission = prisma.permissions;
 const Role = prisma.roles;
 const User = prisma.users;
 
 export default (permission: any) =>
   async (req: Request, res: Response, next: NextFunction) => {
-    const access = await Permission.findUnique({
-      where: { name: permission },
-      include: {
-        roles: {
-          select: {
-            role: {
-              select: {
-                id: true,
-                name: true,
-              },
-            },
-          },
-        },
-      },
-    });
-
     // 1. We have to check if this user, who is provided by `req.user`, has permissions to the `permission` argument provided from the `routes` folder
     // The `roles` works an intermediary between `users` and `permissions`
     // 2. We have to access to the permissions of the user via the `roles_permissions` model
@@ -70,51 +53,7 @@ export default (permission: any) =>
       (user) => user.permission.name === permission
     );
 
-    console.log("hasUserPermission", hasUserPermission);
-
-    async function hasPermission(permission: any) {
-      if (!permission || permission === "undefined") {
-        return false;
-      }
-      const permissions = await Permission.findMany();
-
-      const result = !!permissions
-        .map(({ name }: { name: any }) => name)
-        .includes(permission.name);
-
-      console.log("result", result);
-
-      return result;
-    }
-
-    async function hasPermissionThroughRole(permission: any) {
-      if (!permission || permission === "undefined") {
-        return false;
-      }
-      const roles = await Role.findMany();
-      for await (const item of permission.roles) {
-        if (
-          roles.filter((role) => {
-            role.name === item.role.name;
-          }).length > 0
-        ) {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    async function hasPermissionTo(permission: any) {
-      if (!permission || permission === "undefined") {
-        return false;
-      }
-      return (
-        (await hasPermissionThroughRole(permission)) ||
-        hasPermission(permission)
-      );
-    }
-
-    if (await hasPermissionTo(access)) {
+    if (await hasUserPermission) {
       return next();
     }
 
