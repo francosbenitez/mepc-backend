a7b42089b204dd0bcc669547c7c7cc348357dcc0
francobenitez <franco.benitez@hogarthww.com>
2022-08-03 15:33:57 -0300

    chore: try making authorization work

--- src/middlewares/canAccess.ts
+++ src/middlewares/canAccess.ts
@@ -4,6 +4,7 @@ import type { Request, Response, NextFunction } from "express";
 const prisma = new PrismaClient();
 const Permission = prisma.permissions;
 const Role = prisma.roles;
+const User = prisma.users;
 
 export default (permission: any) =>
   async (req: Request, res: Response, next: NextFunction) => {
@@ -23,45 +24,87 @@ export default (permission: any) =>
       },
     });
 
-    console.log("access", access);
-    console.log("access.roles", access?.roles);
+    const user = await User.findUnique({
+      where: {
+        id: req.user?.id,
+      },
+      include: {
+        roles: {
+          select: {
+            role: {
+              select: {
+                id: true,
+              },
+            },
+          },
+        },
+      },
+    });
 
-    async function hasPermission(permission: any) {
-      if (!permission || permission === "undefined") {
-        return false;
-      }
-      const permissions = await Permission.findMany();
-      return !!permissions
-        .map(({ name }: { name: any }) => name)
-        .includes(permission.name);
-    }
+    console.log("user", user);
+    console.log("user.roles", user?.roles);
+    console.log("user.roles[0].role.id", user?.roles[0].role.id);
 
-    async function hasPermissionThroughRole(permission: any) {
-      if (!permission || permission === "undefined") {
-        return false;
-      }
-      const roles = await Role.findMany();
-      for await (const item of permission.roles) {
-        if (roles.filter((role) => role.name === item.name).length > 0) {
-          return true;
-        }
-      }
-      return false;
-    }
+    // Check if the roleId = 1 has the required permissions
+    const getPermissionsBasedOnRole = await Permission.findMany({
+      where: {
+        id: user?.roles[0].role.id,
+      },
+      // include: {
+      //   permissions: {
+      //     select: {
+      //       permission: {
+      //         select: {
+      //           id: true,
+      //         },
+      //       },
+      //     },
+      //   },
+      // },
+    });
 
-    async function hasPermissionTo(permission: any) {
-      if (!permission || permission === "undefined") {
-        return false;
-      }
-      return (
-        (await hasPermissionThroughRole(permission)) ||
-        hasPermission(permission)
-      );
-    }
+    const getPermissions = await Permission.findMany();
 
-    if (await hasPermissionTo(access)) {
-      return next();
-    }
+    console.log("getPermissions", getPermissions);
+    console.log("getPermissionsBasedOnRole", getPermissionsBasedOnRole);
+    console.log("permission", permission);
+
+    // async function hasPermission(permission: any) {
+    //   if (!permission || permission === "undefined") {
+    //     return false;
+    //   }
+    //   const permissions = await Permission.findMany();
+    //   return !!permissions
+    //     .map(({ name }: { name: any }) => name)
+    //     .includes(permission.name);
+    // }
+
+    // async function hasPermissionThroughRole(permission: any) {
+    //   if (!permission || permission === "undefined") {
+    //     return false;
+    //   }
+    //   const roles = await Role.findMany();
+    //   for await (const item of permission.roles) {
+    //     if (roles.filter((role) => role.name === item.name).length > 0) {
+    //       return true;
+    //     }
+    //   }
+    //   return false;
+    // }
+
+    // async function hasPermissionTo(permission: any) {
+    //   if (!permission || permission === "undefined") {
+    //     return false;
+    //   }
+    //   return (
+    //     (await hasPermissionThroughRole(permission)) ||
+    //     hasPermission(permission)
+    //   );
+    // }
+
+    // if (await hasPermissionTo(access)) {
+    //   return next();
+    // }
     console.error("You do not have the authorization to access this.");
     return sendErrorResponse(
       res,
