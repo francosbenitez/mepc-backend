5dbb3d59851fe3869e8aebaadd7ed0b9f0055fc9
francobenitez <franco.benitez@hogarthww.com>
2022-08-03 21:38:06 -0300

    fix: deploy

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,10 +1,8 @@
-declare namespace Express {
-  export interface Request {
-    user?: {
-      id: any;
-    };
-  }
-  export interface Response {
-    user: any;
-  }
-}
+// declare namespace Express {
+//   export interface Request {
+//     user?: any;
+//   }
+//   export interface Response {
+//     user: any;
+//   }
+// }
