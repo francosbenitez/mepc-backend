3a2e28aedb6753221d8acf35df64f2dc7b0badee
francobenitez <franco.benitez@hogarthww.com>
2022-08-05 11:41:01 -0300

    feat: add authenticated user to the seeder

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -26,6 +26,7 @@ async function main() {
   await seedData(roleData, prisma.roles, "roles");
   await seedData(permissionData, prisma.permissions, "permissions");
 
+  console.log("Seeding admin user ...");
   const adminUser = await prisma.users.create({
     data: {
       username: "admin",
@@ -75,6 +76,58 @@ async function main() {
         role: {
           connect: {
             id: adminRole?.id,
+          },
+        },
+      },
+    });
+  }
+
+  console.log("Seeding authenticated user ...");
+  const authenticatedUser = await prisma.users.create({
+    data: {
+      username: "francosbenitez",
+      email: "francosbenitez@gmail.com",
+      password: bcrypt.hashSync("password", 10),
+    },
+  });
+
+  const authenticatedRole = await prisma.roles.findUnique({
+    where: { name: Constants.ROLE_AUTHENTICATED },
+  });
+  const authenticatedPermissions = await prisma.permissions.findMany({
+    where: {
+      name: {
+        in: [Constants.PERMISSION_VIEW_ARTICLES],
+      },
+    },
+  });
+
+  await prisma.roles_users.create({
+    data: {
+      role: {
+        connect: {
+          id: authenticatedRole?.id,
+        },
+      },
+      user: {
+        connect: {
+          id: authenticatedUser.id,
+        },
+      },
+    },
+  });
+
+  for (const permission of authenticatedPermissions) {
+    await prisma.roles_permissions.create({
+      data: {
+        permission: {
+          connect: {
+            id: permission.id,
+          },
+        },
+        role: {
+          connect: {
+            id: authenticatedRole?.id,
           },
         },
       },
