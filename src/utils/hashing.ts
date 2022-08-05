2f55bec3aacb9c3e09534c9f209cb76cb6a7e8a1
francobenitez <franco.benitez@hogarthww.com>
2022-08-05 11:34:10 -0300

    chore: remove hashing.ts

--- src/utils/hashing.ts
+++
@@ -1,7 +0,0 @@
-// import crypto from "crypto";
-
-// export const hash = (string: string) =>
-//   crypto.createHash("sha256").update(string).digest("base64");
-
-// export const hash_compare = (first_item: string, second_item: string) =>
-//   Object.is(first_item, second_item);
